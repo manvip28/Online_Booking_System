8a9bf0b4dabc674329e736ac81697ac21f3b091f
unknown <manvip2804@gmail.com>
2026-01-04 03:27:29 +0530

    Added Controller Test

--- src/main/java/com/app/booking/dto/request/BookingRequest.java
+++ src/main/java/com/app/booking/dto/request/BookingRequest.java
@@ -2,10 +2,14 @@ package com.app.booking.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingRequest {
     @NotNull
     private Long showId;
